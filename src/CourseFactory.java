ee103c4168a07bbbfd673b089cf6021a9bd878f9
lualunathee <aluasagdatova2005@gmail.com>
2024-12-13 01:09:33 +0600

    gg

--- src/CourseFactory.java
+++ src/CourseFactory.java
@@ -1,4 +1,34 @@
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
 public class CourseFactory {
-
+	
+	private CourseType courseType;
+	
+	public CourseFactory(CourseType courseType) {
+		this.courseType = courseType;
+	}
+	
+	public boolean createCourse(String name, int credits, Vector<String> preRequisites) {
+		
+		String preReq = String.join("->", preRequisites);
+	    String courseInfo = name + '=' + credits + '=' + preReq;
+		
+	    return saveToFile("src\\Data\\courses.txt", courseInfo);
+	    
+	}
+	
+	
+	private static boolean saveToFile(String filePath, String data) {
+		try(BufferedWriter writer new BufferedWriter(new FileWriter(filePath, true))){
+			writer.writer(data);
+			writer.newLine();
+			return true;
+		} catch(IOException e) {
+			System.err.println("Error writing" + filePath + ":" + e.getMessage());
+			return false;
+		}
+		
+	}
 }
--- src/CourseType.java
+++ src/CourseType.java
@@ -1,4 +1,21 @@
 
 public class CourseType {
+	
+	public String typeName;
+	
+	public CourseType(String typeName) {
+		this.typeName = typeName;
+	}
 
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	
+	public String toString() {
+		return typeName;
+	}
 }
