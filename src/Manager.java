905b7dca78093808a451319552df5e54940374c2
lualunathee <aluasagdatova2005@gmail.com>
2024-12-13 02:46:32 +0600

    lalala

---
+++ src/Manager.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+public class Manager extends Employee {
+
+	public Manager(int id, String name, String password, double salary) {
+		
+		super(id, name, password, salary);
+	}
+	
+	public boolean createCourse(String name, int credits, CourseType courseType, Vector<String> preRequisites) {
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter("scr\\Data\\courses.txt", true))){
+			String preReq = String.join("->", preRequisites);
+			writer.write(name + "=" + credits + "=" + courseType.getTypeName() + "=" + preReq);
+			writer.newLine();
+			return true;
+		} catch (IOException e) {
+			System.err.println("Error writing to file" + e.getMessage());
+		} 
+		
+		return false;
+		
+	}
+}
