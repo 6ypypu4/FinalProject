2105c0802f669d04db861fb7979bd66cb0703d24
lualunathee <aluasagdatova2005@gmail.com>
2024-12-13 15:17:00 +0600

    enums

--- src/LessonType.java
+++ src/LessonType.java
@@ -1,24 +1,7 @@
 
-public class LessonType {
+public enum LessonType {
 	
-	public String type;
-
-	public LessonType(String type) {
-		this.type = type;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String toString() {
-		return "Lesson Type:"+ type;
-	}
-	
-	
-
+	LECTURE,
+    PRACTICE,
+    LABORATORY
 }
