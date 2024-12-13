ac8c904eea16af311c10c41dbcd8535e55a477d4
lualunathee <aluasagdatova2005@gmail.com>
2024-12-13 14:06:59 +0600

    курс

--- src/Course.java
+++ src/Course.java
@@ -1,11 +1,20 @@
+import java.util.Vector;
+
 public class Course {
 	
 	public String typeName;
+	public String name;
+	public int credits;
+	public Vector<String> preRequisites;
 	
-	public Course(String typeName) {
+	public Course(String typeName, String name, int credits, Vector<String> preRequisites) {
 		this.typeName = typeName;
+		this.name = name;
+		this.credits = credits;
+		this.preRequisites = preRequisites;
 	}
 
+	
 	public String getTypeName() {
 		return typeName;
 	}
@@ -14,7 +23,38 @@ public class Course {
 		this.typeName = typeName;
 	}
 	
-	public String toString() {
-		return typeName;
+	
+	public String getName() {
+		return name;
 	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getCredits() {
+		return credits;
+	}
+
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+
+
+	public Vector<String> getPreRequisites() {
+		return preRequisites;
+	}
+
+
+	public void setPreRequisites(Vector<String> preRequisites) {
+		this.preRequisites = preRequisites;
+	}
+
+
+	public String toString() {
+		return "Course Type: " + typeName + " Name: " + name + " Credits: " + credits + " Pre-requisites: " + String.join(" ", preRequisites);	
+		}
 }
